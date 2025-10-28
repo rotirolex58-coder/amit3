ec4f07b2dfc75abfa85f7c12b52ff1a90bf7aeea
rotirolex58-coder <rotirolex58@gmail.com>
2025-10-28 11:03:45 +0530

    Updated Transformations.tsx

--- src/pages/Transformations.tsx
+++ src/pages/Transformations.tsx
@@ -52,6 +52,34 @@ const Transformations = () => {
     "https://www.instagram.com/p/C4afxa9SLj9/",
     "https://www.instagram.com/p/C4-ibvRSCFn/",
     "https://www.instagram.com/p/C5lLtSESMIy/",
+    <iframe src="https://www.instagram.com/p/C2uUCCWB8yO/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C4afxa9SLj9/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C4-ibvRSCFn/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C5lLtSESMIy/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C6YqmPFy_sC/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C68sARKyYfv/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C7OtPzCyMc8/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C8Mjfz_hKGJ/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C9UmJv_S-Vj/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C9xcw_8ymwY/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C-KtXHSSaBB/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C_QUQAdSEXG/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C_dGLXnyBZz/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C_5by_sSJVp/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DBvZqwHSF6A/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DC_ruQZSB9b/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DD9kRbqzVPm/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DEZz86BzlEc/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DFFkxd8ykgM/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DGfrl-Tz0a0/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DHa6CbuT3J1/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DJEcRBeSQ52/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DKCSMuYSsXp/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DLZ03ueyTrh/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DMfWI50zt3E/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DNLHhghyaiF/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/DN25Gt75nqn/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
+<iframe src="https://www.instagram.com/p/C19ylpbS4Wn/embed" frameborder="0" allowfullscreen width="400" height="480"></iframe>							
   ];
 
   const getColumns = (width: number) => {
